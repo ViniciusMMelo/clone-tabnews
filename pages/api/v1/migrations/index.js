abaa3d8528b4a468c2f40b619ab21ab5b717d0b0
Vinicius Melo <122281546+ViniciusMMelo@users.noreply.github.com>
2026-01-20 22:01:02 +0000

    fix migrations endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,6 +3,10 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
+  if (request.method !== "GET" && request.method !== "POST") {
+    return response.status(405).json({ "Error 405": "Method not allowed" });
+  }
+
   const dbClient = await database.getNewClient();
 
   const defaultMigrationOptions = {
